b6a28710b5e1c341edc51fc81086d34c1be25b58
Jon Nyman <nymanjon@gmail.com>
2022-08-14 09:49:17 -0700

    Dynamically get search params from request url

--- Soccer/FrontEnd/src/web/js/utils.ts
+++ Soccer/FrontEnd/src/web/js/utils.ts
@@ -1,3 +1,14 @@
 export const isSelected =
     <T extends string>(currentValue: string|undefined) =>
     (value: T) => value === currentValue ? "selected" : null
+
+const searchParamsHandler = {
+  get(obj: any, prop: string) {
+    return obj.searchParams.get(prop)
+  }
+}
+
+export function searchParams<TReturn>(req: Request) : TReturn {
+    let url = new URL(req.url)
+    return new Proxy(url, searchParamsHandler)
+}
