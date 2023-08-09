e82a695155a3456ef40ec083a54dc7be50379679
Jon Nyman <nymanjon@gmail.com>
2023-08-08 20:22:45 -0700

    Code clean up

--- src/web/js/players-edit.js
+++ src/web/js/players-edit.js
@@ -1,20 +1,9 @@
 // @ts-check
 
-(() => {
+document.addEventListener("change", e => {
+    // @ts-ignore
+    let form = e.target?.form
+    if (form instanceof HTMLFormElement)
+        form.submit()
+})
 
-    document.addEventListener("change", e => {
-        // @ts-ignore
-        let form = e.target?.form
-        if (form instanceof HTMLFormElement)
-            form.submit()
-    })
-
-    document.addEventListener("reset-form", e => {
-        /** @type {HTMLFormElement|undefined} */
-        // @ts-ignore
-        let form = e.target?.form
-        if (form instanceof HTMLFormElement)
-            form.reset()
-    })
-
-})();
