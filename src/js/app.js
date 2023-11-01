d41d315cb65fdc45545fb17d8ea1c772da1e5694
Jon Nyman <nymanjon@gmail.com>
2023-11-01 15:34:03 -0700

    Added forgot password page

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,49 @@
 (() => {
 
-document.addEventListener('submit', async e => {
+const doc = document
+
+doc.addEventListener('submit', async e => {
     e.preventDefault()
     const form = e.target
-    let response = await fetch(form.action, {
+    let action = form.action
+    let response = await fetch(action, {
         method: form.method,
         body: new FormData(form)
     })
-    let data = await response.json()
-    if (response.ok) {
+    let data
+    if (response.headers.get('content-type').includes('application/json')) {
+        data = await response.json()
+    }
+    if (!response.ok) {
+        if (data?.msg) {
+            writeMessage(data?.msg)
+        }
+        return
+    }
+
+    if (['login', 'register'].some(x => action.includes(x))) {
         location.href = "/web/?login=success"
         return
     }
-    if (data.msg) {
-        alert(data.msg)
+
+    if (action.includes("forgot-password")) {
+        // alert("Password reset link has been sent to your email")
+        writeMessage("Send me an email that you requested a password reset")
+        return
     }
+
 })
+
+function getHtml(text) {
+    const template = doc.createElement("template")
+    template.innerHTML = text.trim()
+    return template.content
+}
+
+function writeMessage(msg) {
+    let message = getHtml(`<p class="message">${msg}</p>`)
+    doc.querySelector('main').prepend(message)
+}
 
 })()
 
