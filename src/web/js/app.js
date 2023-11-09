802a192e51838de665e71678acc749947935fef6
Jon Nyman <nymanjon@gmail.com>
2023-11-09 16:48:22 -0700

    code clean up

--- src/web/js/app.js
+++ src/web/js/app.js
@@ -1,14 +1,15 @@
 (() => {
 
-let w = window
+const w = window,
+    doc = document
 
-document.addEventListener("user-messages", e => {
-    let template = document.getElementById("toast-template")
-    let toasts = document.getElementById("toasts")
 navigator.serviceWorker.addEventListener('controllerchange', () => {
   w.location.reload()
 })
 
+doc.addEventListener("user-messages", e => {
+    let template = doc.getElementById("toast-template")
+    let toasts = doc.getElementById("toasts")
     for (let message of e.detail) {
         let clone = template.content.cloneNode(true)
         clone.querySelector(".message").textContent = message
@@ -17,21 +18,21 @@ navigator.serviceWorker.addEventListener('controllerchange', () => {
     closeDialogs()
 })
 
-document.addEventListener("hf:completed", e => {
+doc.addEventListener("hf:completed", e => {
     if (e.detail.method === "get") return
-    let count = document.getElementById('get-sync-count-form')
+    let count = doc.getElementById('get-sync-count-form')
     count.requestSubmit()
 })
 
-document.addEventListener("app-theme", e => {
+doc.addEventListener("app-theme", e => {
     let theme = e.detail.theme
-    document.body.classList.remove("light")
-    document.body.classList.remove("dark")
-    document.body.classList.add(theme)
+    doc.body.classList.remove("light")
+    doc.body.classList.remove("dark")
+    doc.body.classList.add(theme)
 })
 
 function closeDialogs() {
-    for (let dialog of document.querySelectorAll("dialog.toast")) {
+    for (let dialog of doc.querySelectorAll("dialog.toast")) {
         let id = setTimeout(() => {
             dialog.remove()
             clearTimeout(id)
@@ -55,20 +56,20 @@ async function sync() {
             method: 'POST',
         })
     if (response.redirected) {
-        document.location = document.location
+        doc.location.reload()
         return
     }
     if (response.headers.get("content-type")?.includes("application/json")) {
         let json = await response.json()
         if (json.messages) {
-            document.dispatchEvent(new CustomEvent("user-messages", {
+            doc.dispatchEvent(new CustomEvent("user-messages", {
                 detail: json.messages
             }))
         }
     }
     let status = response.status
     if (status > 199 && status < 300) {
-        let count = document.getElementById('sync-count')
+        let count = doc.getElementById('sync-count')
         if (count) {
             count.innerHTML = "&#128259;"
         }
