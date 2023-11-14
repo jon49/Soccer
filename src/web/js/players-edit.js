be7a5766c911530cf0c0599f54ab7ea3799eb62e
Jon Nyman <nymanjon@gmail.com>
2023-11-13 21:46:19 -0700

    remove event listener on unload

--- src/web/js/players-edit.js
+++ src/web/js/players-edit.js
@@ -1,9 +1,23 @@
 // @ts-check
 
-document.addEventListener("change", e => {
+((w) => {
+
+// @ts-ignore
+w.app.dispose.push(() => {
+    document.removeEventListener("change", listen)
+})
+
+/**
+* @param {Event} e
+* @returns {void}
+* */
+function listen(e) {
     // @ts-ignore
     let form = e.target?.form
     if (form instanceof HTMLFormElement)
         form.requestSubmit()
-})
+}
 
+document.addEventListener("change", listen)
+
+})(window)
