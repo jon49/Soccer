c7c7fda50c3a7b0994b4c1b13b17fb246313d8b2
Jon Nyman <nymanjon@gmail.com>
2023-11-30 15:29:01 -0700

    Introduce templates for creating positions

--- src/web/pages/positions.html.ts
+++ src/web/pages/positions.html.ts
@@ -1,9 +1,9 @@
 import html, { when } from "../server/html.js"
 import layout from "./_layout.html.js"
 import { Team } from "../server/db.js"
-import { PostHandlers, Route } from "../server/route.js"
+import { PostHandlers, Route, RouteGetHandler } from "../server/route.js"
 import { teamGet } from "../server/repo-team.js"
-import { createArrayOf, createPositiveWholeNumber, createString25, maybe, validateObject } from "../server/validation.js"
+import { createArrayOf, createPositiveWholeNumber, createString25, maybe, required, validateObject } from "../server/validation.js"
 import { queryTeamIdValidator } from "../server/validators.js"
 import { positionGetAll, positionsSave } from "../server/repo-player-game.js"
 import { teamNav } from "./_shared-views.js"
@@ -24,7 +24,14 @@ function render({ team, positions, grid }: PositionView) {
     return html`
 <h2>${team.name} - Positions</h2>
 
+<form
+    id=show-templates-form
+    action="/web/positions?handler=showTemplates&teamId=${team.id}"
+    hf-target="#dialogs"
+    hidden>
+</form>
 <h3>Grid</h3>
+<p>Use a <button form=show-templates-form>template</button>.</p>
 <form
     id=add-grid
     class=row
@@ -55,6 +62,148 @@ ${when(grid.length, () => html`
     }}
 </form>
 `)}`
+}
+
+const templates = {
+    "4": [
+        [1, 2, 1],
+        [1, 1, 2],
+    ],
+    "7": [
+        [1, 3, 2, 1],
+        [1, 2, 3, 1],
+        [2, 2, 2, 1],
+    ],
+    "9": [
+        [2, 3, 3, 1],
+        [3, 3, 2, 1],
+        [2, 4, 2, 1],
+        [3, 2, 3, 1],
+    ],
+    "11": [
+        [2, 4, 4, 1],
+        [3, 3, 4, 1],
+        [2, 5, 3, 1],
+        [1, 3, 2, 4, 1],
+        [3, 4, 3, 1],
+    ],
+}
+
+type PossibleTemplatePlayerCounts = keyof typeof templates
+
+function getNumberOfPlayers(numberOfPlayers: number) : PossibleTemplatePlayerCounts {
+    if (["4", "7", "9", "11"].includes(""+numberOfPlayers)) {
+        // @ts-ignore
+        return ""+numberOfPlayers
+    }
+    return "4"
+}
+
+function getCircles({ count, y, total }: { count: number, y: number, total: number }) {
+    let x = Math.round(1e4/(count + 1))/100
+    let cy = 90 * (y / total)
+    let circles = ""
+    for (let i = 0; i < count; i++) {
+        circles += `<circle cx="${x * (i + 1)}%" cy="${cy}%" r="3%" fill="#fd6b00" />`
+    }
+    return circles
+}
+
+function getTemplate(positions: number[]) {
+    return html`
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+    $${
+    function* rows() {
+        let total = positions.length
+        let y = 1
+        for (let count of positions) {
+            yield getCircles({ count, y, total })
+            y++
+        }
+    }
+}
+</svg>
+`
+}
+
+function getTemplates(teamId: number, numberOfPlayers: number) {
+    let playerCount = getNumberOfPlayers(numberOfPlayers)
+    let grid = templates[playerCount]
+    return grid.map((x, i) => html`
+<form
+    method=post
+    class=inline
+    action="/web/positions?teamId=${teamId}&handler=createFormation"
+    hf-target="main"
+    >
+<button class=bg>${getTemplate(x)}</button>
+<input type=hidden name=index value="${i}">
+<input type=hidden name=numberOfPlayers value="${numberOfPlayers}">
+</form>`
+    )
+}
+
+function getPositionTemplates(teamId: number) {
+    return html`
+    <dialog class=modal is=x-dialog show-modal close-event="user-messages">
+    <h2 class=inline>Position Templates</h2> 
+    <form class=inline method=dialog>
+        <button value=cancel>Cancel</button>
+    </form>
+
+    <form
+        action="/web/positions?handler=getTemplates&teamId=${teamId}"
+        onchange="this.requestSubmit()"
+        onload="this.requestSubmit()"
+        hf-target="#templates"
+        >
+        <select name=numberOfPlayers>
+            <option value=4>4</option>
+            <option value=7>7</option>
+            <option value=9>9</option>
+            <option value=11>11</option>
+        </select>
+    </form>
+    <div id=templates class=rows></div>
+    </dialog>`
+}
+
+const positionNames = [
+    "Striker",
+    "Forward",
+    "Midfield",
+    "Fullback",
+    "Goalkeeper",
+]
+
+const positionNameSuffix = {
+    "5": ["L", "LC", "C", "RC", "R"],
+    "4": ["L", "LC", "RC", "R"],
+    "3": ["L", "C", "R"],
+    "2": ["L", "R"],
+    "1": [""],
+}
+
+function setPositions(grid: number[]) : string[][] {
+    let names = [...positionNames]
+    if (grid.length === 4) {
+        names.splice(0, 1)
+    } else if (grid.length === 3) {
+        names.splice(0, 1).splice(3, 1)
+    }
+    let positions = []
+    for (let i = 0; i < grid.length; i++) {
+        let column = grid[i]
+        let name = names[i]
+        let row = []
+        // @ts-ignore
+        for (let suffix of positionNameSuffix[""+column]) {
+            row.push(`${name} ${suffix}`)
+        }
+        positions.push(row)
+    }
+
+    return positions
 }
 
 const positionValidator = {
@@ -108,19 +257,59 @@ const postHandlers : PostHandlers = {
         o.positions = positions
         await positionsSave(teamId, o)
         return { body: null, status: 204 }
-    }
+    },
+
+    async createFormation({ query, data }) {
+        let { teamId } = await validateObject(query, queryTeamIdValidator)
+        let { index, numberOfPlayers } = await validateObject(data, {
+            index: createPositiveWholeNumber("Index"),
+            numberOfPlayers: createPositiveWholeNumber("Number of Players"),
+        })
+
+        let o = await positionGetAll(teamId)
+        let playerCount = getNumberOfPlayers(numberOfPlayers)
+        let grid = await required(templates[playerCount][index], "Template not found!")
+        let positions = setPositions(grid)
+
+        await positionsSave(teamId, {
+            ...o,
+            grid,
+            positions,
+        })
+
+        return render(await start(query))
+    },
 }
 
-const route : Route = {
-    route: /\/positions\/$/,
+const getHandlers : RouteGetHandler = {
     async get({ query }) {
         const result = await start(query)
         return layout({
             main: render(result),
             nav: teamNav(result.team.id),
             title: `Positions - ${result.team.name} (${result.team.year})`,
+            scripts: [ "/web/js/x-dialog.js" ],
         })
     },
+
+    async showTemplates({ query }) {
+        let { teamId } = await validateObject(query, queryTeamIdValidator)
+        return getPositionTemplates(teamId)
+    },
+
+    async getTemplates({ query }) {
+        let { teamId, numberOfPlayers } = await validateObject(query, {
+            ...queryTeamIdValidator,
+            numberOfPlayers: createPositiveWholeNumber("Number of Players"),
+        })
+
+        return html`${getTemplates(teamId, numberOfPlayers)}`
+    }
+}
+
+const route : Route = {
+    route: /\/positions\/$/,
+    get: getHandlers,
     post: postHandlers,
 }
 
