0ab67a5d77c7005e3fd273610d32c807c94c9e60
Jon Nyman <nymanjon@gmail.com>
2026-03-01 15:48:52 -0700

    Auto format vs code.

--- src/web/pages/stats/stats.page.ts
+++ src/web/pages/stats/stats.page.ts
@@ -5,91 +5,91 @@ import { playerPointsView } from "./_player-points-view.js"
 import { percentagePlayed } from "./_percentage-played.js"
 
 const {
-    html,
-    layout,
-    repo: { teamGet },
-    validation: { validateObject, queryTeamIdValidator },
-    views: { teamNav },
+  html,
+  layout,
+  repo: { teamGet },
+  validation: { validateObject, queryTeamIdValidator },
+  views: { teamNav },
 } = self.sw
 
 async function render(query: any) {
-    let { teamId } = await validateObject(query, queryTeamIdValidator)
-    let team = await teamGet(teamId)
+  let { teamId } = await validateObject(query, queryTeamIdValidator)
+  let team = await teamGet(teamId)
 
-    let href = (handler: string) =>
-        `href="?teamId=${teamId}&handler=${handler}"`
+  let href = (handler: string) =>
+    `href="?teamId=${teamId}&handler=${handler}"`
 
-    return html`
-    <h2 class=inline>${team.name} — Stats</h2>
-    <a href="/web/stats/edit?teamId=${teamId}" target="_self">Edit</a>
+  return html`
+  <h2 class=inline>${team.name} — Stats</h2>
+  <a href="/web/stats/edit?teamId=${teamId}" target="_self">Edit</a>
 
-    <div style="margin-bottom: 1em;">
-        <div
-            id=statButtons
-            class=grid
-            style="--grid-item-width: 150px;">
-            ${[
-                ["timePlayed", "Time Played"],
-                ["percentagePlayed", "Percentage of Games Played"],
-                ["activitiesPerformed", "Player Points"],
-            ].map(([handler, label]) =>
-             html`<a id="${handler}" $${href(handler)} role="button">$${label}</a>`)
-            }
-        </div>
-    </div>
+  <div style="margin-bottom: 1em;">
+  <div
+    id=statButtons
+    class=grid
+    style="--grid-item-width: 150px;">
+    ${[
+      ["timePlayed", "Time Played"],
+      ["percentagePlayed", "Percentage of Games Played"],
+      ["activitiesPerformed", "Player Points"],
+    ].map(([handler, label]) =>
+      html`<a id="${handler}" $${href(handler)} role="button">$${label}</a>`)
+    }
+  </div>
+  </div>
 
-    <div id=statTables></div>`
+  <div id=statTables></div>`
 }
 
 const getHandler: RouteGetHandler = {
-    async get({ query }) {
-        return layout({
-            head: `<style>
-            table.sticky {
-                th {
-                    position: sticky;
-                    top: 0;
-                    z-index: 2;
-                }
+  async get({ query }) {
+    return layout({
+      head: `<style>
+table.sticky {
+  th {
+    position: sticky;
+    top: 0;
+    z-index: 2;
+  }
 
-                td:first-child, th:first-child {
-                    position: sticky;
-                    left: 0;
-                    z-index: 1;
-                }
-            }
-            </style>`,
-            main: await render(query),
-            nav: teamNav(+query.teamId),
-            title: "Player Points"
-        })
-    },
+  td:first-child, th:first-child {
+    position: sticky;
+    left: 0;
+    z-index: 1;
+  }
+}
+    </style>`,
+      main: await render(query),
+      nav: teamNav(+query.teamId),
+      title: "Player Points"
+    })
+  },
 
-    async timePlayed({ query }) {
-        let playersView = await StatsView.create(query)
-        return html`
-        <template id="statTables" hz-swap="prepend">${timePlayedView(playersView)}</template>
-        <template id=timePlayed></template>`
-    },
+  async timePlayed({ query }) {
+    let playersView = await StatsView.create(query)
+    return html`
+  <template id="statTables" hz-swap="prepend">${timePlayedView(playersView)}</template>
+  <template id=timePlayed></template>`
+  },
 
-    async percentagePlayed({ query }) {
-        let playersView = await StatsView.create(query)
-        return html`
-        <template id="statTables" hz-swap="prepend">${percentagePlayed(playersView)}</template>
-        <template id=percentagePlayed></template>`
-    },
+  async percentagePlayed({ query }) {
+    let playersView = await StatsView.create(query)
+    return html`
+  <template id="statTables" hz-swap="prepend">${percentagePlayed(playersView)}</template>
+  <template id=percentagePlayed></template>`
+  },
 
 
-    async activitiesPerformed({ query }) {
-        let playersView = await StatsView.create(query)
-        return html`
-        <template id="statTables" hz-swap="prepend">${playerPointsView(playersView)}</template>
-        <template id=activitiesPerformed></template>`
-    }
+  async activitiesPerformed({ query }) {
+    let playersView = await StatsView.create(query)
+    return html`
+  <template id="statTables" hz-swap="prepend">${playerPointsView(playersView)}</template>
+  <template id=activitiesPerformed></template>`
+  }
 }
 
 const router: RoutePage = {
-    get: getHandler
+  get: getHandler
 }
 
 export default router
